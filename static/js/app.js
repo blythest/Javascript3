f69aa635baa7ddf32d297e773109bc7b1f6018fb
Hackbright Student <info@hackbrightacademy.com>
2014-03-04 12:21:38 -0800

    functional todo app

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,33 @@
 $(document).ready(function(){
+
+    var submitButton = document.getElementById("form_submit");
+    
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault();
+        
+        $.ajax({
+            url: "/",
+            method: "POST",
+            data: $("form#todo_list_form").serialize(),
+        }).done(function(data){
+        var nameValue = document.getElementById('todo_id');
+        nameValue.value = '';
+        }).fail(function(){
+            alert('fail!');
+        });
+
+    });
+
+    var getListsFromServer = function() {
+        $.ajax({
+            url:'/todo_lists/poll',
+            method: 'GET',
+        }).done(function(data) {
+       
+            var listElements = document.getElementById('mystuff');
+            listElements.innerHTML = data;
+
+        });
+    };
+    setInterval(getListsFromServer, 2000);
 });
