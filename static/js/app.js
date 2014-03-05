589c90a1ca1ec24aefc8a2c9529fd24ec4abe187
Hackbright Student <info@hackbrightacademy.com>
2014-03-04 16:23:40 -0800

    working todo list with ability to add individual items.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     var submitButton = document.getElementById("form_submit");
+
+    if (submitButton !== null ) {
     
     submitButton.addEventListener('click', function(event){
         event.preventDefault();
@@ -10,24 +12,61 @@ $(document).ready(function(){
             method: "POST",
             data: $("form#todo_list_form").serialize(),
         }).done(function(data){
-        var nameValue = document.getElementById('todo_id');
-        nameValue.value = '';
+            var nameValue = document.getElementById('todo_id');
+            nameValue.value = '';
+            var listElements = document.getElementById('todo-list');
+            listElements.innerHTML = data;
+
+            
         }).fail(function(){
             alert('fail!');
         });
 
     });
 
-    var getListsFromServer = function() {
+} else {
+    var itemSubmitButton = document.getElementById("submit");
+    
+    itemSubmitButton.addEventListener('click', function(event){
+        event.preventDefault();
         $.ajax({
-            url:'/todo_lists/poll',
-            method: 'GET',
-        }).done(function(data) {
-       
+            url: " ",
+            method: "POST",
+            data: $("form#todo-item-form").serialize(),
+        }).done(function(data){
+            var nameValue = document.getElementById('task_id');
+            nameValue.value = '';
             var listElements = document.getElementById('mystuff');
             listElements.innerHTML = data;
 
+            
+        }).fail(function(){
+            alert('fail!');
         });
-    };
-    setInterval(getListsFromServer, 2000);
+
+    });
+    }
+
+
 });
+
+
+    // var getListsFromServer = function() {
+    //     $.ajax({
+    //         url:'/todo_lists/poll',
+    //         method: 'GET',
+    //     }).done(function(data) {
+    //         var listElements = document.getElementById('mystuff');
+    //         listElements.innerHTML = data;
+
+    //     });
+    // };
+    // setInterval(getListsFromServer, 2000);
+
+                // //nameValue.value = what's in the form input
+            // var newList = document.createElement('div');
+            // newList.className = 'todo-list';
+            // var listText = document.createTextNode(nameValue.value);
+            // newList.appendChild(listText);
+            // var currentList = document.getElementById('mystuff');
+            // currentList.appendChild(newList);
